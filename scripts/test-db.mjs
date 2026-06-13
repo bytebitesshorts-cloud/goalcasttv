decd26952ccee480b6de0a3519654943cac91772
nznazmul <xprttanvir@gmail.com>
2026-06-14 03:18:49 +0600

    chore: remove all unused utility scripts from project

--- scripts/test-db.mjs
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-console.log("URI:", process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 5000 })
-  .then(() => { console.log('Connected!'); process.exit(0); })
-  .catch(err => { console.error('Error:', err); process.exit(1); });
